8d715104d130e9fe44715b71f8b6dff6e85f844f
shabooboo86 <128689197+shabooboo86@users.noreply.github.com>
2023-09-25 14:44:52 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'ShaBooBoo Blog',
+  domain: 'shabooboo.info',
+  author: 'ShaBooBoo',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'share technology',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
